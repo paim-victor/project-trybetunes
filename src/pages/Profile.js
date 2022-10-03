4f307c8d301cffabc77e3ce68e569d4b42c732ba
Paim <victor-paim@hotmail.com>
2022-10-03 15:54:00 -0300

    requisito 1

---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile">
+        <div data-testid="page-profile-edit" />
+      </div>
+    );
+  }
+}
+
+export default Profile;
