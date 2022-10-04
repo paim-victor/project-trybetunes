c770315a3e5965b441177ae2ca7c80d6c9a8142a
Paim <victor-paim@hotmail.com>
2022-10-04 18:00:31 -0300

    a

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,77 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Carregando from '../components/Carregando';
+import { createUser } from '../services/userAPI';
 
-class Login extends Component {
+export default class Login extends Component {
+  state = {
+    carregando: false,
+    desativado: true,
+    entrada: '',
+  };
+
+  botaoValid = () => {
+    const { entrada } = this.state;
+    const MIN_LENG = 3;
+    if (entrada.length >= MIN_LENG) {
+      this.setState({
+        desativado: false,
+      });
+    }
+  };
+
+  entradaValida = ({ a }) => {
+    const { nome, valor } = a;
+    this.setState({
+      [nome]: valor,
+    }, this.botaoValid);
+  };
+
+  click = () => {
+    const { history } = this.props;
+    const { entrada } = this.state;
+    this.setState({
+      carregando: true,
+    }, async () => {
+      await createUser({ nome: entrada });
+      history.push('/search');
+    });
+  };
+
   render() {
+    const { desativado, carregando } = this.state;
+    if (carregando) {
+      return (<Carregando />);
+    }
+
     return (
       <div data-testid="page-login">
         Login
+        <label htmlFor="login-input">
+          <input
+            id="login-input"
+            nome="entrada"
+            type="text"
+            data-testid="login-nome-input"
+            onChange={ this.entradaValida }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ desativado }
+          onClick={ this.click }
+        >
+          Entrar
+
+        </button>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      
+        <div data-testid="page-profile-edit">
+        <h1>ProfileEdit</h1>
+        </div>
+    );
+  }
+}
+
+export default ProfileEdit;
